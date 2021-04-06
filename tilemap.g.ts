7a1e03accf5b8fd077f5ea6a5715d436dc598fee
MatesAndrasko <71689778+MatesAndrasko@users.noreply.github.com>
2021-04-06 19:14:26 +0200

    final

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,8 +3,6 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
@@ -24,6 +22,8 @@ namespace myTiles {
     export const tile11 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -152,8 +152,6 @@ namespace myTiles {
         switch(helpers.stringTrim(name)) {
             case "myTile0":
             case "tile1":return tile1;
-            case "myTile":
-            case "tile2":return tile2;
             case "start":
             case "tile3":return tile3;
             case "jidlo":
@@ -173,6 +171,8 @@ namespace myTiles {
             case "Three":
             case "tile11":return tile11;
             case "transparency16":return transparency16;
+            case "win_portal":
+            case "tile2":return tile2;
         }
         return null;
     })
